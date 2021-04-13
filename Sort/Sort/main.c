d178cacd3de364f18b490f4f5afb463a44b2d7f5
seochan99 <gmlcks0513@naver.com>
2021-04-13 22:45:18 +0900

    Merge

--- Sort/Sort/main.c
+++ Sort/Sort/main.c
@@ -1,34 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void InsertSort(int arr[], int n)
+void MergeTwoArea(int arr[], int left, int mid, int right)
 {
-    int i,j;
-    int insData;
+    int fIdx =left;
+    int rIdx = mid + 1;
+    int i;
     
-    for(i=1;i<n;i++)
+    //  병합 한 결과를 담을 배열 sortArr의 동적 할당!
+    int * sortArr = (int*)malloc(sizeof(int)*(right+1));
+    int sIdx =left;
+    
+    while(fIdx<=mid&&rIdx<=right)
     {
-        insData = arr[i]; // 정렬 대상을 ins에 저장
+        // 병합 할 두 영역의 데이터를 비교하여, 정렬 순서대로 sortArr에 하나씩 옮겨 담는다.
+        if(arr[fIdx]<=arr[rIdx])
+            sortArr[sIdx] =arr[fIdx++];
+        else
+            sortArr[sIdx]= arr[rIdx++];
         
-        for(j=i-1;j>=0;j--)
-        {
-            if(arr[j]>insData)
-                arr[j+1]=arr[j];//비교. 대상을 한 칸 뒤로 밀기
-            else
-                break; //삽입 위치 찾음 !
-        }
-        arr[j+1] = insData;
+        sIdx++;
+    }
+    
+    if(fIdx > mid) // 배열의 앞부분이 모두 SortArr에 옮겨 졌다면
+    {
+        //배열 뒷부분에 남은 데이터들을 모두 sortArr에 그대로 옮긴다.
+        for(i=rIdx;i<=right;i++,sIdx++)
+                sortArr[sIdx]= arr[i];
+    }
+    else{ // 배열의 뒷부분이 모두 옮겨졌다면
+        for(i=fIdx;i<=mid;i++,sIdx++) //배열의 앞부분에 남은 데이터들을 그대로 옮긴다.
+        sortArr[sIdx] = arr[i];
+    }
+    
+    for(i=left; i<=right; i++)
+    arr[i] = sortArr[i];
+    
+    free(sortArr);
+    
+}
+
+void MergeSort(int arr[],int left,int right)
+{
+    int mid;
+    
+    if(left < right)
+    {
+        //중간 지점을 게산한다.
+        mid = (left+right)/2;
+        
+        MergeSort(arr, left, mid);
+        MergeSort(arr, mid+1, right);
+        
+        MergeTwoArea(arr, left, mid, right);
     }
 }
 
 int main(void)
 {
-    int arr[5] = {5,3,2,4,1};
+    int arr[7]={3,2,4,1,7,6,5};
     int i;
     
-    InsertSort(arr, sizeof(arr)/sizeof(int));
+    MergeSort(arr,0,sizeof(arr)/sizeof(int)-1);
     
-    for(i=0;i<5;i++)
-    printf("%d ",arr[i]);
+    for (i=0; i<7; i++) {
+        printf("%d ",arr[i]);
+    }
     
     printf("\n");
     return 0;
