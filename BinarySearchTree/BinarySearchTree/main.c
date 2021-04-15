18ac88ef14c7138b78044e1193834d5ebf4507f8
seochan99 <gmlcks0513@naver.com>
2021-04-15 21:13:53 +0900

    Searacj

---
+++ BinarySearchTree/BinarySearchTree/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  BinarySearchTree
+//
+//  Created by 서희찬 on 2021/04/15.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ Search/Search/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int ISearch(int ar[],int first,int last, int target)
+{
+    int mid;
+    
+    if(ar[first]>target || ar[last]<target)
+        return -1;
+    
+    //이진 탐색과의 차이점을 반영한 문장
+    mid = ((double)(target-ar[first])/(ar[last]-ar[first])*(last-first))+first;
+    if(ar[mid]==target)
+        return mid;
+    else if(target<ar[mid])
+        return ISearch(ar, first, mid-1, target);
+    else
+        return ISearch(ar, mid+1, last, target);
+}
+
+
+int main(void)
+{
+    int arr[]={1,3,5,7,9};
+    int idx;
+    
+    idx = ISearch(arr, 0, sizeof(arr)/sizeof(int)-1, 2);
+    if(idx == -1)
+        printf("탐색 실패 \n");
+    else
+        printf("타겟 저장 인덱스 : %d \n",idx);
+    idx = ISearch(arr, 0, sizeof(arr)/sizeof(int)-1, 10);
+    if(idx==-1)
+        printf("탐색 실패 \n");
+    else
+        printf("타겟 저장 인덱스 : %d \n",idx);
+    return 0;
+}
