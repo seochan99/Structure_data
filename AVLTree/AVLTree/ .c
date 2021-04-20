cb99d22a4ad91b3ed31fd04d1b161b6e898ff1be
seochan99 <gmlcks0513@naver.com>
2021-04-20 10:01:47 +0900

    gg

--- AVLTree/AVLTree/ .c
+++ AVLTree/AVLTree/ .c
@@ -1,40 +1,14 @@
+//
+//  main.c
+//  AVLTree
+//
+//  Created by 서희찬 on 2021/04/19.
+//
+
 #include <stdio.h>
-#include "BinaryTree3.h"    
-#include "BinarySearchTree3.h"
 
-int main(void)
-{
-    BTreeNode * avlRoot;
-    BTreeNode * clNode;        // current left node
-    BTreeNode * crNode;        // current right node
-    BSTMakeAndInit(&avlRoot);
-
-    BSTInsert(&avlRoot, 1);
-    BSTInsert(&avlRoot, 2);
-    BSTInsert(&avlRoot, 3);
-    BSTInsert(&avlRoot, 4);
-    BSTInsert(&avlRoot, 5);
-    BSTInsert(&avlRoot, 6);
-    BSTInsert(&avlRoot, 7);
-    BSTInsert(&avlRoot, 8);
-    BSTInsert(&avlRoot, 9);
-
-    printf("∑Á∆Æ ≥ÎµÂ: %d \n", GetData(avlRoot));
-
-    clNode = GetLeftSubTree(avlRoot);
-    crNode = GetRightSubTree(avlRoot);
-    printf("øﬁ¬ 1: %d, ø¿∏•¬ 1: %d \n", GetData(clNode), GetData(crNode));
-
-    clNode = GetLeftSubTree(clNode);
-    crNode = GetRightSubTree(crNode);
-    printf("øﬁ¬ 2: %d, ø¿∏•¬ 2: %d \n", GetData(clNode), GetData(crNode));
-
-    clNode = GetLeftSubTree(clNode);
-    crNode = GetRightSubTree(crNode);
-    printf("øﬁ¬ 3: %d, ø¿∏•¬ 3: %d \n", GetData(clNode), GetData(crNode));
-
-    clNode = GetLeftSubTree(clNode);
-    crNode = GetRightSubTree(crNode);
-    printf("øﬁ¬ 4: %d, ø¿∏•¬ 4: %d \n", GetData(clNode), GetData(crNode));
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
     return 0;
 }
