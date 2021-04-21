d9eee0eb07585c01b65110adcad61536b40ad580
seochan99 <gmlcks0513@naver.com>
2021-04-21 22:44:06 +0900

    RE2

--- AVLTree/AVLRebalance.c
+++ AVLTree/AVLRebalance.c
@@ -55,6 +55,20 @@ BTreeNode * RotateLR(BTreeNode*bst) // LR회전을 담당하는 함수
     return RotateLL(pNode); // LL회전
 }
 
+BTreeNode * RotateRL(BTreeNode*bst) // LR회전을 담당하는 함수
+{
+    BTreeNode * pNode; // parent Node
+    BTreeNode * cNode; // child Node
+    
+    //pNode, cNode 가 RL회전을 위해 적절한 위치를 가리키게한다.
+    pNode = bst;
+    cNode = GetRightSubTree(pNode);
+    
+    //실제 RL회전을 담당하는 두 개의 문장
+    ChangeRightSubTree(pNode, RotateLL(cNode)); // 부분적 LL회전
+    return RotateRR(pNode); // LL회전
+}
+
 int GetHeight(BTreeNode * bst)
 {
     int leftH; // left Height
@@ -86,3 +100,26 @@ int GetHeightDiff(BTreeNode * bst)
     return lsh - rsh;
 }
 
+BTreeNode * Rebalance(BTreeNode **pRoot)
+{
+    int hDiff = GetHeightDiff(*pRoot); // 균형 인수 계산
+    
+    //균형 인수가 +2 이상이면 LL/LR 상태이다.
+    if(hDiff>1) // +2이상이면(왼쪽 서브 트리 방향으로 높이가 2 이상 크다면)
+    {
+        if(GetHeightDiff(GetLeftSubTree(*pRoot))>0)
+            *pRoot = RotateLL(*pRoot);
+        else
+            *pRoot = RotateLR(*pRoot);
+    }
+    //균형 인수가 -2 이하이면 RR/RL 상태이다.
+    if(hDiff<-1) // 오른쪽 서브 트리 방향으로 2 이상 크다면,
+    {
+        if(GetHeightDiff(GetRightSubTree(*pRoot))<0)
+            *pRoot = RotateRR(*pRoot);
+        else
+            *pRoot = RotateRL(*pRoot);
+    }
+    
+    return *pRoot;
+}
