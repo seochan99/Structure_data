2e8863e63b6828169675b22bf0fc06b0b212fec9
seochan99 <gmlcks0513@naver.com>
2021-04-28 14:47:27 +0900

    [[

---
+++ Practice/Practice/main.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+
+
+// 1번 문제
+//int main(void)
+//{
+//    int num1,num2;
+//    printf("더할 두 수를 입력 해주세요");
+//    scanf("%d %d",&num1,&num2);
+//    printf("두 수의 합 : %d\n",num1+num2);
+//
+//
+//    return 0;
+//}
+
+
+//2
+//int main(void){
+//    int num1;
+//    while(1)
+//    {
+//        printf("성적을 입력해주세요(0이상100이하의 정수) : ");
+//        scanf("%d",&num1);
+//        if(0<=num1&&num1<=100)
+//            break;
+//        else
+//            printf("0이상 100이하의 정수를 입력해주세요. \n");
+//    }
+//    if(num1>=90)
+//        printf("A\n");
+//    else if(90>num1&&num1>=80)
+//        printf("B\n");
+//    else if(80>num1&&num1>=70)
+//        printf("C\n");
+//    else if(70>num1&&num1>=60)
+//        printf("D\n");
+//    else
+//        printf("F\n");
+//    return 0;
+//}
+
+
+//3
+//int main(void)
+//{
+//    int num1;
+//    while(1)
+//    {
+//        printf("1이상 100이하의 정수를 입력해주세요 ");
+//        scanf("%d",&num1);
+//        if(1<=num1&&num1<=100)
+//            break;
+//        else
+//            printf("1이상 100이하의 정수를 입력해주세요. \n");
+//    }
+//    for(int i=0;i<=num1;i++)
+//    {
+//
+//        for(int j = 0; j<i;j++)
+//        {
+//            printf("*");
+//
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
+
+
+//4
+//
+//int main(void)
+//{
+//    int num1;
+//    int result = 0;
+//    int max=0;
+//    int min=10;
+//    printf("입력을 원하는 숫자의 갯수를 입력해주세요 : ");
+//    scanf("%d ",&num1);
+//
+//    for(int i=0;i<=num1;i++)
+//    {
+//        int num2;
+//        scanf("%d",&num2);
+//        result += num2;
+//
+//        if(num2>max)
+//            max = num2;
+//        if(min>num2)
+//            num2 = min; // 수정
+//    }
+//
+//    printf("총 합 : %d ", result);
+//    printf("최댓값 : %d ", max);
+//    printf("최솟값 : %d ", min);
+//
+//
+//
+//    return 0;
+//}
+
