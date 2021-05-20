b940ea7f46b24d3a5a8b388bc80dd7e907edc6c0
seochan99 <gmlcks0513@naver.com>
2021-05-20 12:43:34 +0900

    213

--- Assigment4_multi/Assigment4_multi/MultiQueue.h
+++ Assigment4_multi/Assigment4_multi/MultiQueue.h
@@ -1,13 +1,42 @@
-//
-//  MultiQueue.h
-//  Assigment4_multi
-//
-//  Created by 서희찬 on 2021/05/17.
-//
-
 #ifndef MultiQueue_h
 #define MultiQueue_h
 
 #include <stdio.h>
+
+#define TRUE    1
+#define FALSE    0
+
+typedef char name[100];
+
+typedef struct _node
+{
+    Data name;
+    struct _node * next;
+} Node;
+
+typedef struct _lQueue
+{
+    Node * front;
+    Node * rear;
+    int numOfNode;
+    int numOfQueue;
+} LQueue;
+
+typedef LQueue Queue;
+
+void QueueInit(Queue * pq); // 큐 초기화 , 베리어도 ..?
+
+int QIsEmpty(Queue * pq); // 빈 상태 - >
+
+
+void EnBarrier(Queue * pq, int num); // 베리어 삽입
+
+void Enqueue(Queue * pq, Data name); // 큐 삽입
+Data Dequeue(Queue * pq); // 큐 삭제
+
+#endif
+
+
+
 
 #endif /* MultiQueue_h */
